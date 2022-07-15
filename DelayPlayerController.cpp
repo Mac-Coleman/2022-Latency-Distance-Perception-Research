a4e93e07a1f5fa6ba2a47b301a68e908f7b48076
Mac Coleman <williamcormaccoleman@gmail.com>
2022-07-15 15:53:44 -0500

    Initial Commit

---
+++ DelayPlayerController.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "DelayPlayerController.h"
+
+ADelayPlayerController::ADelayPlayerController()
+	: Super()
+{
+
+}
+
+void ADelayPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Camera = Cast<UCameraComponent>(GetPawn()->GetDefaultSubobjectByName("Camera"));
+
+	if (Camera == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("Failed to find Camera"));
+	}
+
+	LastTransform = FTransform();
+
+	FReport Empty;
+	Empty.Transform = FTransform();
+	Empty.TimeStamp = GetGameTimeSinceCreation();
+
+	AgingInputs.Add(Empty);
+
+	TicksConsumed = 0;
+	Total = 0.0f;
+}
+
+void ADelayPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float CurrentTime = GetGameTimeSinceCreation();
+
+	FRotator Orientation;
+	FVector Position;
+	FVector Scale = FVector(1.0f, 1.0f, 1.0f);
+
+	UHeadMountedDisplayFunctionLibrary::GetOrientationAndPosition(Orientation, Position);
+	//You must add "HeadMountedDisplay" to <Project Name>.Build.cs under <Project Name>/Source/<Project Name> to avoid Link Error
+	//PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+
+	FTransform NewTransform = FTransform(Orientation, Position, Scale);
+
+	FReport NewReport;
+	NewReport.Transform = NewTransform;
+	NewReport.TimeStamp = CurrentTime;
+
+	AgingInputs.Add(NewReport); //Always add new reports (Never forget to add them)
+	
+
+	if (CurrentTime - AgingInputs[0].TimeStamp > AddedLatency)
+	{
+		Total += CurrentTime - AgingInputs[0].TimeStamp;
+		TicksConsumed++;
+
+		Camera->SetRelativeTransform(AgingInputs[0].Transform);
+
+		AgingInputs.RemoveAt(0); //Only the oldest input if the latency time has passed.
+		//Current implementation tends to overshoot target latency significantly.
+		//I might try to calculate the instantaneous latency and control it with a really simple proportional controller.
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, DeltaTime, FColor::Blue, TEXT("Aging Inputs: ") + FString::FromInt(AgingInputs.Num()));
+	GEngine->AddOnScreenDebugMessage(-1, DeltaTime, FColor::Blue, TEXT("Average Latency: ") + FString::SanitizeFloat(Total / TicksConsumed));
+}
---
+++ DelayPlayerController.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/CameraComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DelayPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTVR_API ADelayPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ADelayPlayerController();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	void UpdateRotation(float DeltaTime);
+	void SetTransform(FTransform Target);
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	float AddedLatency = 0 * 0.001; //Desired Added Latency (seconds)
+	//You can either set the latency hear and recompile each time or you can make a BluePrint class that extends from this
+	//so you can change the latency a bit quicker.
+
+	FTransform LastTransform;
+	
+private:
+	// These two values are only used for calculating average latency.
+	int TicksConsumed;
+	float Total;
+
+	//Stores the aging inputs.
+	TArray<FReport> AgingInputs;
+
+	//Just the reference to the camera.
+	UCameraComponent* Camera;
+};
+
+USTRUCT()
+struct FReport
+{
+	GENERATED_BODY()
+
+	FTransform Transform;
+	float TimeStamp;
+};
